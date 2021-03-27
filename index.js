5b7bbaa84c2f844a30a072a02d3a09665722857d
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-27 17:16:15 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+let formData = {
+    name: "Steve",
+    email:"steve@steve.com"
+}
+
+const destinationUrl = "http://localhost:3000/users"
+
+let configObject = {
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+}
+
+function submitData() {
+    return fetch(destinationUrl, configObject)
+    .then(response=>response.json())
+    .then(e=>addId (e.id))
+    .catch(error=>addId(error.message))
+};
+
+function addId(id) {
+    let newId = document.createElement("p");
+    document.body.appendChild(newId);
+    newId.innerHTML = id
+}
